55314a3062ddc0394bb208860015d4eb72d3ad4b
down8 <62386825+down8@users.noreply.github.com>
2021-03-05 00:43:20 +0800

    Add files via upload

---
+++ home.js
@@ -0,0 +1,9 @@
+document.writeln("<script>");
+document.writeln("var _hmt = _hmt || [];");
+document.writeln("(function() {");
+document.writeln("  var hm = document.createElement(\'script\');");
+document.writeln("  hm.src = \'https://hm.baidu.com/hm.js?300ba2c5f0d46ece417348237ef2a46d\';");
+document.writeln("  var s = document.getElementsByTagName(\'script\')[0]; ");
+document.writeln("  s.parentNode.insertBefore(hm, s);");
+document.writeln("})();");
+document.writeln("</script>");
